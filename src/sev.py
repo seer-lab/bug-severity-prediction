4ba550cb732eb9a22e30ebb28439c4f929b2a138
Jude Arokiam <jude.arokiam@uoit.net>
2019-03-16 15:23:21 -0400

    Add code to test Doc2Vec model using K-Folds

---
+++ src/sev.py
@@ -0,0 +1,321 @@
+# data loading
+import numpy as np
+import pandas as pd
+import math
+
+# preprocessing (paragragh vectors)
+import gensim
+import multiprocessing
+from functools import reduce
+
+# MLP Classifier
+from sklearn.neural_network import MLPClassifier
+
+# Evaluation
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
+from pprint import pprint
+from itertools import combinations
+
+def load_data(datasets):
+	'''Reads in and formats data from the list of datasets given.
+
+	Args:
+		datasets: A list of Dataset objects (Dataset object defined below)
+	
+	Returns:
+		A numpy array that is the concatenation of the numpy arrays for all the
+		given datasets.
+	'''
+	return np.concatenate([_load_data(dataset) for dataset in datasets])
+
+def _load_data(dataset):
+	'''Helper function to load_data. Reads in file for a single dataset given.
+	
+	Args:
+		dataset: A Dataset object (Dataset object defined below)
+	
+	Returns:
+		A numpy array with the required columns from dataset.
+	'''
+	df = pd.read_csv(dataset.path, sep=',', encoding='ISO-8859-1')
+	raw_data = np.array(df)
+
+	# get the columns for Subject and Severity Rating
+	extract_cols = [1, 2]
+	del_cols = np.delete(np.arange(raw_data.shape[1]), extract_cols)
+	data = np.delete(raw_data, del_cols, axis=1)
+
+	# check for possible NaN severity values
+	del_rows = []
+	for i in range(len(data)):
+		if math.isnan(data[i][1]):
+			del_rows.append(i)
+
+	# delete rows that contain NaN severity values
+	if len(del_rows) > 0:
+		data = np.delete(data, del_rows, axis=0)
+
+	# add column for project id
+	dataset_size = len(data)
+	project_id_column = [dataset.project_id for i in range(dataset_size)]
+	data = np.insert(data, 2, project_id_column, axis=1)
+
+	# filter dataset percent
+	if dataset.percent != 1:
+		size = len(data)
+		train_size = int(size * dataset.percent)
+		if dataset.train:
+			# delete row not needed from last row
+			data = np.delete(data, slice(train_size, size), axis=0)
+
+			#print('Range to delete: ', train_size, '-', size)
+		else:
+			# delete rows not needed from first row
+			data = np.delete(data, slice(0, train_size), axis=0)
+
+			#print('Range to delete: ', 0, '-', train_size)
+
+		#print('Size of Dataset: ', size)
+		#print('Size of remaining', len(data))
+
+	return data
+
+
+def preprocess(train_data, test_data):
+	'''Generate paragragh vectors using doc2vec from given data
+
+	Args:
+		train_data: 2-D numpy array that represents the data to train with
+		test_data:  2-D numpy array that represents the data to test with
+
+	Returns:
+		Preprocessed data ready to be fed into a classifer. 
+
+		X_train: list of paragragh vectors to train with
+		Y_train: actual severity ratings for X_train 
+		X_test:  list of paragragh vectors to test against
+		Y_test:  actual severity ratings for X_test
+	'''
+	# construct testing and training corpora (plural or corpus)
+	train_corpus = list(_read_corpus(train_data))
+	test_corpus = list(_read_corpus(test_data, tokens_only=True))
+
+	cores = multiprocessing.cpu_count()
+
+	# instantiate Doc2Vec object
+	# !! switch to epoch 4000 or 40000 when done testing 
+	model_DM = gensim.models.doc2vec.Doc2Vec(vector_size=500, min_count=15, epochs=400, workers=cores,  dm=1, dm_concat=1 )
+	model_DBOW = gensim.models.doc2vec.Doc2Vec(vector_size=500, min_count=15, epochs=400, workers=cores, dm=0)
+
+	# build a vocabulary
+	model_DM.build_vocab(train_corpus)
+	model_DBOW.build_vocab(train_corpus)
+
+	# train Doc2Vec
+	model_DM.train(train_corpus, total_examples=model_DM.corpus_count, epochs=model_DM.epochs)
+	model_DBOW.train(train_corpus, total_examples=model_DBOW.corpus_count, epochs=model_DBOW.epochs)
+
+	# create test and train sets using Doc2Vec output
+	#X_train = [(list(model_DM.docvecs[i]) + list(model_DBOW.docvecs[i])) for i in range(len(train_data))]
+	X_train = []
+	for i in range(len(train_data)):
+		doc_vec = ((list(model_DM.docvecs[i]) + list(model_DBOW.docvecs[i])))
+		#project_vec = [reduce((lambda x, y: x + y), doc_vec), train_data[i][2]]
+		doc_vec.append(train_data[i][2])
+		X_train.append(doc_vec)
+	Y_train = [doc[1] for doc in train_data]
+
+	#X_test = [(list(model_DM.infer_vector(test_corpus[i])) + list(model_DBOW.infer_vector(test_corpus[i]))) for i in range(len(test_data))]
+	X_test = []
+	for i in range(len(test_data)):
+		doc_vec = (list(model_DM.infer_vector(test_corpus[i])) + list(model_DBOW.infer_vector(test_corpus[i])))
+		#project_vec = [reduce((lambda x, y: x + y), doc_vec), test_data[i][2]]
+		doc_vec.append(test_data[i][2])
+		X_test.append(doc_vec)
+	Y_test = [doc[1] for doc in test_data]
+
+	return X_train, Y_train, X_test, Y_test
+
+
+def _read_corpus(data, tokens_only=False):
+	'''Helper function to prepare data for hidden doc2vec model in preprocess
+	   function.
+
+	Args:
+		data: 2-D numpy array that can be training or test data
+		token_only: boolean value used to determine if training/test is data
+
+	Returns:
+		A list of lists filled with tokens if test data. If training data, then
+		a list of list of TaggedDocuments (doc2vec class) will be returned. 
+	'''
+	for i, line in enumerate(data):
+		if tokens_only:
+			yield gensim.utils.simple_preprocess(line[0])
+		else:
+			# For training data, add tags
+			yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line[0]), [i])
+
+			
+class ASP():
+	def __init__(self, X_train, Y_train, X_test, Y_test):
+		self.X_train, self.Y_train = X_train, Y_train
+		self.X_test, self.Y_test = X_test, Y_test
+		self.classifier = MLPClassifier(solver='sgd', learning_rate='adaptive', learning_rate_init=1, alpha = 0.01, max_iter=10000)
+		
+	def fit(self):
+		self.classifier.fit(self.X_train, self.Y_train)
+
+		#df_results = pd.DataFrame(data=np.zeros(shape=(0,3)), columns = ['classifier', 'train_score', 'test_score'] )
+		#train_score = self.classifier.score(self.X_train, self.Y_train)
+		#test_score = self.classifier.score(self.X_test, self.Y_test)
+		 
+		#print  (classifier.predict_proba(X_test))
+		#print  (classifier.predict(X_test))
+		 
+		#df_results.loc[1,'classifier'] = "MLP"
+		#df_results.loc[1,'train_score'] = train_score
+		#df_results.loc[1,'test_score'] = test_score
+		#print(df_results)
+		
+
+	def predict(self):
+		prediction = self.classifier.predict(self.X_test)
+
+		#matrix = confusion_matrix(self.Y_test, prediction, labels=[1, 2, 3, 4])
+		#print(matrix)
+
+		prf1 = precision_recall_fscore_support(y_true=self.Y_test, y_pred=prediction, average='weighted')
+		print('Average')
+		print('Precision | Recall | F-Score')
+		pprint(prf1)
+
+		prf2 = precision_recall_fscore_support(y_true=self.Y_test, y_pred=prediction, average=None, labels=[2, 3, 4, 5])
+		print('')
+		print('By Severity')
+		print('Precision | Recall | F-Score')
+		pprint(prf2)
+
+
+class Dataset():
+	def __init__(self, path, project_id, percent=1, train=True):
+		self.path = path
+		self.project_id = project_id
+		self.percent = percent
+		self.train = train
+
+class Experiment():
+	def __init__(self, train, test):
+		self.train = train
+		self.test = test
+
+	def run(self):
+		# load data
+		train_data = load_data(self.train)
+		test_data = load_data(self.test)
+		#print('loaded data')
+
+		# create training and testing sets
+		X_train, Y_train, X_test, Y_test = preprocess(train_data, test_data)
+		#print('preprocessed')
+
+		# classify
+		classifier = ASP(X_train, Y_train, X_test, Y_test)
+		classifier.fit()
+		#print('trained classifier')
+
+		classifier.predict()
+		#print('prediction complete')		
+
+# SCRIPT START -----------------------------------------------------------------
+
+# dataset variables
+a = '../dataset/raw/pitsA.csv'
+b = '../dataset/raw/pitsB.csv'
+c = '../dataset/raw/pitsC.csv'
+d = '../dataset/raw/pitsD.csv'
+e = '../dataset/raw/pitsE.csv'
+f = '../dataset/raw/pitsF.csv'
+
+# ------------------------------------------------------------------------------
+# experiments using SEVERIS parameters and environment
+# ------------------------------------------------------------------------------
+# datasets
+datasets = (a, b, c, d, e, f)
+#datasets = [a]
+
+#percent of test data that will be in training data
+import time
+from sklearn.model_selection import KFold
+
+print(time.ctime(time.time()))
+start = time.time()
+
+experiment_count = 1
+for dataset in datasets:
+	print('-----EXPERIMENT ', experiment_count, ' START-----')
+	print('Dataset: ', dataset)
+	data = load_data([Dataset(dataset, project_id=1, percent=1, train=False)])
+
+	kf = KFold(n_splits=10)
+	num_fold = 1
+	for train, test in kf.split(data):
+		print(' ')
+		print('Fold: ', num_fold)
+		# create training and testing sets for fold
+		train_data = [data[i] for i in train]
+		test_data = [data[i] for i in test]
+		X_train, Y_train, X_test, Y_test = preprocess(train_data, test_data)
+
+		# classify
+		classifier = ASP(X_train, Y_train, X_test, Y_test)
+		classifier.fit()
+
+		classifier.predict()
+		num_fold += 1
+	
+	print('-----EXPERIMENT ', experiment_count, ' END-------')
+	experiment_count +=1
+	print('')
+
+print(time.ctime(time.time()))
+print('TOTAL RUNTIME: ', time.time()-start, 's')
+print('')
+
+'''
+percent_range = [1]
+for per in percent_range:
+
+	for test_set in [a, b, c, d, e, f]
+		project_id = 1
+
+
+
+		# set up training data and test data for experiment
+		for datasets in train_datasets:
+			project_id = 1
+			pits_train = []
+			
+			# training data
+			for dataset in datasets:
+				pits_train.append(Dataset(dataset, project_id, percent=1, train=True))
+				project_id += 1
+			if per != 1:
+				pits_train.append(Dataset(f, project_id, percent=per, train=True))
+			
+			# test data
+			pits_test = [Dataset(f, project_id, percent=per, train=False)]
+
+			# run experiment
+			
+			if per == 1:
+				print('Percent of test data included in training: ', 0)
+			else:
+				print('Percent of test data included in training: ', per)
+			print('Training Data: ', datasets)
+			experiment = Experiment(pits_train, pits_test)
+			experiment.run()
+			
+'''
+
